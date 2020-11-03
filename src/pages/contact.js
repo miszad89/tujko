d42dea670c69b8005be13e8d13ac8a0547b7976e
miszad89 <miszad89@gmail.com>
2020-11-03 10:13:20 +0100

    Add contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,20 @@
 import React from "react"
 // import styled from "styled-components"
 import Layout from "../components/layout"
+import Article from "../components/article"
 
 const HomePage = () => (
   <Layout>
-      
+    <Article>
+      <h2>Kontakt</h2>
+      <h3>TUJKO S.C.</h3>
+      <p>Adres: Trakt Św. Wojciecha 467, 80-007 GDAŃSK</p>
+      <p>Tel./Fax: 58 309 03 46 lub 58 324 44 50</p>
+      <p>e-mail: tujko@tujko.pl</p>
+      <h3>GODZINY OTWARCIA:</h3>
+      <p>Poniedziałek - Piątek: 7:00 - 18:00</p>
+      <p>Sobota: 8:00 - 12:00</p>
+    </Article>
   </Layout>
 )
 
